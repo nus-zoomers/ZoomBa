c65d72414ff88a46686524c4202ca52b4d5b84e6
eksinyue <xinyuehehxy@gmail.com>
2021-01-08 17:51:43 +0800

    add select theme options

--- src/routes/home/components/ThemeSelection.tsx
+++ src/routes/home/components/ThemeSelection.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import lightThemeImage from '../assets/theme_light.png';
+import darkThemeImage from '../assets/theme_dark.png';
 
 const ThemeSelection = () => {
+  const [theme, setTheme] = useState('Light');
+
+  const handleSelectTheme = (newTheme: string) => {
+    setTheme(newTheme);
+  };
+
   return (
     <>
       <h2 className="config-label" style={{ marginTop: 0 }}>
         Theme
       </h2>
       <div className="config-theme-selection">
-        <p>Font Size: XXX</p>
-        <p>Font Color: XXX</p>
-        <p>Other configs: XXX</p>
+        <button
+          type="button"
+          className={
+            theme === 'Light'
+              ? 'config-theme-option-active'
+              : 'config-theme-option'
+          }
+          onClick={() => handleSelectTheme('Light')}
+        >
+          <img src={lightThemeImage} alt="light theme" />
+        </button>
+        <button
+          type="button"
+          className={
+            theme === 'Dark'
+              ? 'config-theme-option-active'
+              : 'config-theme-option'
+          }
+          onClick={() => handleSelectTheme('Dark')}
+        >
+          <img src={darkThemeImage} alt="dark theme" />
+        </button>
       </div>
     </>
   );
