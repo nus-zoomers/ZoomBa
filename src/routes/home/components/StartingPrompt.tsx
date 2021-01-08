0b0f9593344f3dd8d5046df73fa09d46bc5381f9
Hanming Zhu <hanming@huawen.com>
2021-01-08 15:38:52 +0800

    Add title for Theme and add button hover effects

--- src/routes/home/components/StartingPrompt.tsx
+++ src/routes/home/components/StartingPrompt.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StartingPrompt = () => {
   return (
     <>
-      <h2 className="config-starting-label">Starting Prompt</h2>
+      <h2 className="config-label">Starting Prompt</h2>
       <textarea
         className="config-starting-textarea"
         placeholder="Enter your starting prompt here!"
--- src/routes/home/components/ThemeSelection.tsx
+++ src/routes/home/components/ThemeSelection.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const ThemeSelection = () => {
   return (
-    <div className="config-theme-selection">
-      <p>Font Size: XXX</p>
-      <p>Font Color: XXX</p>
-      <p>Other configs: XXX</p>
-    </div>
+    <>
+      <h2 className="config-label">Theme</h2>
+      <div className="config-theme-selection">
+        <p>Font Size: XXX</p>
+        <p>Font Color: XXX</p>
+        <p>Other configs: XXX</p>
+      </div>
+    </>
   );
 };
 
