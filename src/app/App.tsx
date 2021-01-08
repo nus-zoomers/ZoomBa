f58f8676d31799ec6fa48d5945030ff7cdb947d1
Hanming Zhu <hanming@huawen.com>
2021-01-08 13:17:05 +0800

    Restructure project and migrate home view

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from '../routes/home';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
-import './App.global.css';
+
+import App from './app';
+import './index.global.css';
 
 render(<App />, document.getElementById('root'));
